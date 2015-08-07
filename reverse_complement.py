937e815a354425bb3a136280870dd555c1a10c33
Brian Hall <brian@reallymany.com>
2015-08-06 15:25:13 -1000

    checking for illegal characters

--- reverse_complement.py
+++ reverse_complement.py
@@ -19,6 +19,16 @@ def reverse_complement(seq):
     reversed_seq = seq[::-1]
     return complement(reversed_seq)
 
+# Get input
 original_sequence = raw_input("Enter the sequence to be reverse complemented: ")
+
+# Check for invalid characters
+VALID_CHARACTERS = "ACGT"
+for base in original_sequence:
+    if base not in VALID_CHARACTERS:
+        print("'%s' is not a valid sequence." % original_sequence)
+        exit()
+
+# Print reverse complement
 new_sequence = reverse_complement(original_sequence)
 print(new_sequence)
